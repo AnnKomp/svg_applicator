05da00c29b8c6494c4e5afc70eab8fa35b0aee11
AnnKomp <anna.kompaniets@etu.univ-smb.fr>
2024-05-03 11:18:57 +0200

    changement svg vers cimetiere

--- app/page.tsx
+++ app/page.tsx
@@ -28,7 +28,7 @@ const FichierSVG = () => {
         onChangeValue={handleChangeValue}
       >
         <svg width={700} height={700} xmlns="http://www.w3.org/2000/svg">
-          <image xlinkHref="/papillon.svg" width="700" height="700" />
+          <image xlinkHref="/plan_detaille_cimetiere.svg" width="700" height="700" />
         </svg>
       </ReactSVGPanZoom>
     </div>
