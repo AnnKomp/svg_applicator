dac7790add75c1ad989cc1f0cde635a12cb5112c
AnnKomp <anna.kompaniets@etu.univ-smb.fr>
2024-05-11 14:12:09 +0200

    adieu les tests (ils sont commentés)

--- app/page.tsx
+++ app/page.tsx
@@ -76,48 +76,27 @@ const FichierSVG = () => {
     return { x: xRotated, y: yRotated };
   }
   
-  var latRD = 48.631283; var lonRD = 2.347382; //rond droit 
-
+  /* var latRD = 48.631283; var lonRD = 2.347382; //rond droit 
   var latRG = 48.631504; var lonRG = 2.346355; // rond gauche
-  
   var latEG = 48.631261; var lonEG = 2.343769; // entrée gauche demicercle
-
   var latTL = 48.632848; var lonTL = 2.343715; // top left corner
-
   var latCB = 48.630756; var lonCB = 2.346026; // carrefour bas
-
   var latC =  48.631444; var lonC = 2.345624; //doit etre centre
-
   var latCHD = 48.632216; var lonCHD = 2.345094; //cercle en haut de l'eglise
-
   var latTN = 48.630571; var lonTN = 2.347068; // a coté de tombe de Noureev
-  
   var latCBG = 48.631039; var lonCBG = 2.344579; // carrefour en bas à gauche
-
-/*   var lat = 48.631381; var lon = 2.345083;
- */
-
   var latHG = 48.632389; var lonHG = 2.344288; 
 
   var { x :xEG, y: yEG } = convert(latEG, lonEG);
-
   var { x :xRD, y :yRD } = convert(latRD, lonRD);
-
   var { x :xRG, y :yRG } = convert(latRG, lonRG);
-
   var { x :xTL, y :yTL } = convert(latTL, lonTL);
-
   var {x : xCB, y: yCB} = convert(latCB, lonCB);
-
   var {x : xC, y: yC} = convert(latC, lonC);
-
   var {x : xHG, y: yHG} = convert(latHG, lonHG);
-
   var {x : xCHD, y: yCHD} = convert(latCHD, lonCHD);
-
   var {x : xTN, y: yTN} = convert(latTN, lonTN);
-
-  var {x : xCBG, y: yCBG} = convert(latCBG, lonCBG);
+  var {x : xCBG, y: yCBG} = convert(latCBG, lonCBG); */
 
   let userX = null;
   let userY = null;
@@ -149,7 +128,7 @@ const FichierSVG = () => {
       >
         <svg width={MAP_WIDTH} height={MAP_HEIGHT} xmlns="http://www.w3.org/2000/svg">
           <image xlinkHref="/plan_detaille_cimetiere.svg" width={MAP_WIDTH} height={MAP_HEIGHT} />
-          <circle cx={xEG} cy={yEG} r="5" fill="red" />
+          {/* <circle cx={xEG} cy={yEG} r="5" fill="red" />
           <circle cx={xRD} cy={yRD} r="5" fill="green" />
           <circle cx={xRG} cy={yRG} r="5" fill="red" />
           <circle cx={xTL} cy={yTL} r="5" fill="red" />
@@ -159,12 +138,12 @@ const FichierSVG = () => {
           <circle cx={xCHD} cy={yCHD} r="5" fill="red" />
           <circle cx={xTN} cy={yTN} r="5" fill="red" />
           <circle cx={xCBG} cy={yCBG} r="5" fill="red" />
-
+ */}
           {userX !== null && userY !== null && (
-            <circle cx={userX} cy={userY} r="5" fill="blue" />
+            <circle cx={userX} cy={userY} r="5" fill="red" />
           )} 
 
-          <circle cx={centerX} cy={centerY} r="5" fill="blue" />
+          {/* <circle cx={centerX} cy={centerY} r="5" fill="blue" /> */}
         </svg>
       </ReactSVGPanZoom>
       <GeolocationComponent />
