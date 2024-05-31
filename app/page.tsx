2ae732c4d9d0225369401010205184c99e4bef33
AnnKomp <anna.kompaniets@etu.univ-smb.fr>
2024-05-31 14:47:06 +0200

    plan devient p^lus petit si ecran est petit

--- app/page.tsx
+++ app/page.tsx
@@ -10,12 +10,8 @@ const MAP_WIDTH = 926.59839;
 const MAP_HEIGHT = 566.15918;
 const centerX = MAP_WIDTH / 2;
 const centerY = MAP_HEIGHT / 2;
-const plusX = MAP_WIDTH / 4 * 3;
-const minX = MAP_WIDTH / 4;
-const plusY = MAP_HEIGHT / 4 * 3;
-const minY = MAP_HEIGHT / 4; 
-const rotationDegrees = -17;  // rotation de la carte
-const rotationRadians = rotationDegrees * Math.PI / 180; 
+const rotationDegrees = -17; // rotation de la carte
+const rotationRadians = rotationDegrees * Math.PI / 180;
 const minAccuracy = 10;
 
 const latMax = 48.632868;  // top left latitude (48.632861)
@@ -32,7 +28,7 @@ interface SelectedPersonnes {
 const FichierSVG = () => {
   const [tool, setTool] = useState(TOOL_NONE);
   const [value, setValue] = useState({});
-  const [userPosition, setUserPosition] = useState<{ latitude: number; longitude: number; accuracy: number;} | null>(null);
+  const [userPosition, setUserPosition] = useState<{ latitude: number; longitude: number; accuracy: number; } | null>(null);
   const [error, setError] = useState<string | null>(null);
   const [loading, setLoading] = useState<boolean>(true);
   const [tombes, setTombes] = useState<Tombe[]>([]);
@@ -40,6 +36,7 @@ const FichierSVG = () => {
   const [parcours, setParcours] = useState<Parcours[]>([]);
   const [selectedParcoursId, setSelectedParcoursId] = useState<string>('1');
   const [selectedPersonnes, setSelectedPersonnes] = useState<SelectedPersonnes>({});
+  const [isSmallScreen, setIsSmallScreen] = useState<boolean>(false);
 
   useEffect(() => {
     const fetchDataPersonnes = async (parcoursId: string) => {
@@ -89,8 +86,8 @@ const FichierSVG = () => {
             setLoading(false);
             if (newPosition.accuracy > minAccuracy) {
               console.log("Current accuracy: " + newPosition.accuracy);
-              setTimeout(getLocation, 5000); 
-            } 
+              setTimeout(getLocation, 5000);
+            }
           },
           (error) => {
             setError(error.message);
@@ -105,31 +102,59 @@ const FichierSVG = () => {
     };
 
     getLocation();
+
+    const handleResize = () => {
+      setIsSmallScreen(window.innerWidth < 768);
+    };
+
+    handleResize(); // Initial check
+    window.addEventListener('resize', handleResize);
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+
   }, [selectedParcoursId]);
 
-  function convert(lat: number, lon: number): { x: number, y: number } { 
+  function convert(lat: number, lon: number): { x: number, y: number } {
     const calculX = ((lon - lonMin) / (lonMax - lonMin)) * MAP_WIDTH;
     const calculY = ((latMax - lat) / (latMax - latMin)) * MAP_HEIGHT;
 
-    let x = calculX - (Math.abs(centerX - calculX) - (Math.abs(centerX - calculX) /1.1));
-    let y = calculY - (Math.abs(centerY - calculY) - (Math.abs(centerY - calculY) /1.6));
+    let x = calculX - (Math.abs(centerX - calculX) - (Math.abs(centerX - calculX) / 1.1));
+    let y = calculY - (Math.abs(centerY - calculY) - (Math.abs(centerY - calculY) / 1.6));
 
     if (calculX >= centerX) {
-      x = calculX + (Math.abs(centerX - calculX) - (Math.abs(centerX - calculX) /1.45));
+      x = calculX + (Math.abs(centerX - calculX) - (Math.abs(centerX - calculX) / 1.45));
     }
-    if(calculX >= plusX) {
-      x = calculX + (Math.abs(centerX - calculX) - (Math.abs(centerX - calculX) /1.35));
+    if (calculX >= centerX) {
+      x = calculX + (Math.abs(centerX - calculX) - (Math.abs(centerX - calculX) / 1.35));
     }
 
     if (calculY >= centerY) {
-      y = calculY + (Math.abs(centerY - calculY) - (Math.abs(centerY - calculY) /1.4 ));
+      y = calculY + (Math.abs(centerY - calculY) - (Math.abs(centerY - calculY) / 1.4));
     }
-  
+
     const xRotated = Math.cos(rotationRadians) * (x - centerX) - Math.sin(rotationRadians) * (y - centerY) + centerX;
     const yRotated = Math.sin(rotationRadians) * (x - centerX) + Math.cos(rotationRadians) * (y - centerY) + centerY;
-  
+
     return { x: xRotated, y: yRotated };
   }
+
+  const recalculateCoordinates = (x: number, y: number, currentWidth: number, currentHeight: number) => {
+    const scaleX = currentWidth / MAP_WIDTH;
+    const scaleY = currentHeight / MAP_HEIGHT;
+    return { x: x * scaleX, y: y * scaleY };
+  };
+
+  const recalculateWidth = (w: number, currentWidth: number) => {
+    const scaleX = currentWidth / MAP_WIDTH;
+    return  w * scaleX;
+  };
+
+  const recalculateHeight = (h: number, currentHeight: number) => {
+    const scaleY = currentHeight / MAP_HEIGHT;
+    return h * scaleY;
+  };
 
   let userX = null;
   let userY = null;
@@ -163,51 +188,69 @@ const FichierSVG = () => {
     }));
   };
 
+  const chunkArray = (arr: Personne[], chunkSize: number) => {
+    const chunks = [];
+    for (let i = 0; i < arr.length; i += chunkSize) {
+      chunks.push(arr.slice(i, i + chunkSize));
+    }
+    return chunks;
+  };
+
+  const personnesChunks = chunkArray(personnes, 10);
+
   return (
     <div className="container mx-auto p-4">
       <h1 className="mb-4 text-xl md:text-2xl font-bold">
         PLAN DE LA CIMETIERE DE STE GENEVIEVE DES BOIS
       </h1>
-      <ReactSVGPanZoom    
-        width={MAP_WIDTH}
-        height={MAP_HEIGHT}
-        tool={tool}
-        onChangeTool={handleToolChange}
-        value={value}
-        onChangeValue={handleChangeValue}
-        detectAutoPan={false}
-        detectWheel={false}
-      >
-        <svg width={MAP_WIDTH} height={MAP_HEIGHT} xmlns="http://www.w3.org/2000/svg">
-          <image xlinkHref="/plan_detaille_cimetiere.svg" width={MAP_WIDTH} height={MAP_HEIGHT} />
-          
-          {userX !== null && userY !== null && (
-            <circle cx={userX} cy={userY} r={rayon} fill="red" />
-          )} 
+      <div className={`w-full h-auto ${isSmallScreen ? 'max-w-full' : 'max-w-4xl'} mx-auto`}>
+        <ReactSVGPanZoom
+          width={isSmallScreen ? MAP_WIDTH / 2 : MAP_WIDTH}
+          height={isSmallScreen ? MAP_HEIGHT / 2 : MAP_HEIGHT}
+          tool={tool}
+          onChangeTool={handleToolChange}
+          value={value}
+          onChangeValue={handleChangeValue}
+          detectAutoPan={false}
+          detectWheel={false}
+        >
+          <svg width={isSmallScreen ? MAP_WIDTH / 2 : MAP_WIDTH}
+          height={isSmallScreen ? MAP_HEIGHT / 2 : MAP_HEIGHT}
+           xmlns="http://www.w3.org/2000/svg">
+            <image xlinkHref="/plan_detaille_cimetiere.svg" width={isSmallScreen ? MAP_WIDTH / 2 : MAP_WIDTH}
+          height={isSmallScreen ? MAP_HEIGHT / 2 : MAP_HEIGHT}/>
 
-          {tombes.map((tombe) => {
-            const width = tombe.vertical ? 3.5 : 6;
-            const height = tombe.vertical ? 6 : 3.5;
-            const relatedPersonne = personnes.find(personne => personne.tombe === tombe.id);
-            const nomSite = relatedPersonne ? relatedPersonne.nom_site : 'unknown';
-            return (
-              relatedPersonne && selectedPersonnes[Number(relatedPersonne.id)] && (
-                <rect 
-                  key={tombe.id}
-                  id={nomSite}
-                  x={tombe.x} 
-                  y={tombe.y}
-                  width={width}
-                  height={height}
-                  fill="red"
-                  opacity={0.8}
-                  onClick={() => handleRectClick(nomSite)}
-                />
-              )
-            );
-          })}
-        </svg>
-      </ReactSVGPanZoom>
+            {userX !== null && userY !== null && (
+              <circle cx={userX} cy={userY} r={rayon} fill="red" />
+            )}
+
+            {tombes.map((tombe) => {
+              const currentWidth = isSmallScreen ? MAP_WIDTH / 2 : MAP_WIDTH;
+              const currentHeight = isSmallScreen ? MAP_HEIGHT / 2 : MAP_HEIGHT;
+              const { x, y } = recalculateCoordinates(tombe.x, tombe.y, currentWidth, currentHeight);
+              const width = tombe.vertical ? recalculateWidth(3.5,currentWidth) : recalculateWidth(6,currentWidth);
+              const height = tombe.vertical ? recalculateHeight(6,currentHeight) : recalculateHeight(3.5,currentHeight);
+              const relatedPersonne = personnes.find(personne => personne.tombe === tombe.id);
+              const nomSite = relatedPersonne ? relatedPersonne.nom_site : 'unknown';
+              return (
+                relatedPersonne && selectedPersonnes[Number(relatedPersonne.id)] && (
+                  <rect
+                    key={tombe.id}
+                    id={nomSite}
+                    x={x}
+                    y={y}
+                    width={width}
+                    height={height}
+                    fill="red"
+                    opacity={0.8}
+                    onClick={() => handleRectClick(nomSite)}
+                  />
+                )
+              );
+            })}
+          </svg>
+        </ReactSVGPanZoom>
+      </div>
 
       {loading && <p className="text-center text-gray-500">Loading...</p>}
       {error && <p className="text-center text-red-500">{error}</p>}
@@ -225,21 +268,25 @@ const FichierSVG = () => {
 
       <div className="mt-4 p-4 border border-gray-300 bg-gray-50 rounded">
         <h2 className="text-lg font-medium text-gray-700">Personnes dans le parcours</h2>
-        <ul>
-          {personnes.map(personne => (
-            <li key={personne.id} className="mt-2">
-              <label className="inline-flex items-center">
-                <input
-                  type="checkbox"
-                  checked={!!selectedPersonnes[Number(personne.id)]}
-                  onChange={() => handleCheckboxChange(Number(personne.id))}
-                  className="form-checkbox h-5 w-5 text-indigo-600"
-                />
-                <span className="ml-2 text-gray-700">{personne.nom}</span>
-              </label>
-            </li>
+        <div className="flex flex-wrap">
+          {personnesChunks.map((chunk, chunkIndex) => (
+            <ul key={chunkIndex} className="w-full sm:w-1/2 md:w-1/3 lg:w-1/4">
+              {chunk.map(personne => (
+                <li key={personne.id} className="mt-2">
+                  <label className="inline-flex items-center">
+                    <input
+                      type="checkbox"
+                      checked={!!selectedPersonnes[Number(personne.id)]}
+                      onChange={() => handleCheckboxChange(Number(personne.id))}
+                      className="form-checkbox h-5 w-5 text-indigo-600"
+                    />
+                    <span className="ml-2 text-gray-700">{personne.nom}</span>
+                  </label>
+                </li>
+              ))}
+            </ul>
           ))}
-        </ul>
+        </div>
       </div>
 
       {userPosition && (
